0472b9a4fb2a6688b1662d0def3a029938540e4d
Ahmed Allam <ahmed.allam@cafu.com>
2025-11-26 00:32:14 +0200

    Add price update model for updating the prices

---
+++ PriceTracker/Modules/Feed/Model/PriceUpdate.swift
@@ -0,0 +1,13 @@
+//
+//  PriceUpdate.swift
+//  PriceTracker
+//
+//  Created by Ahmed Allam on 26/11/2025.
+//
+
+import Foundation
+
+struct PriceUpdate: Codable {
+    let symbol: String
+    let price: Double
+}
