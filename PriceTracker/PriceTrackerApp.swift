79a375bffa1ad226a133b3478f00ddd784e9e870
Ahmed Allam <ahmed.allam@cafu.com>
2025-11-25 23:38:51 +0200

    Initial Commit

---
+++ PriceTracker/PriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceTrackerApp.swift
+//  PriceTracker
+//
+//  Created by Ahmed Allam on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
